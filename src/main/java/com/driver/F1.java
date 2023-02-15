ef11c3708b9134448307f5380ea181f609ab6e6e
Ankit Banerjee <ankitbanerjee0211@gmail.com>
2023-02-15 20:44:30 +0530

    Submission - 4

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,7 +4,7 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
-        super(name, 4, 2, 6, isManual, "F1", 2);
+        super(name, 4, 2, 6, isManual, "formula", 2);
     }
 
     public void accelerate(int rate){
@@ -18,25 +18,25 @@ public class F1 extends Car {
          * speed 201-250: gear 5
          * speed more than 250: gear 6
          */
-        int currSp = getCurrentSpeed();
-        newSpeed = currSp + currSp*rate/100;
+
+        newSpeed = getCurrentSpeed() + getCurrentSpeed()*rate/100;
 
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
             stop();
-            changeGear(1);
-        } else if(newSpeed >= 1 && newSpeed <= 50){
-            changeGear(1);
+            setCurrentGear(1);
+        }else if(newSpeed >= 1 && newSpeed <= 50){
+            setCurrentGear(1);
         }else if(newSpeed >= 51 && newSpeed <= 100){
-            changeGear(2);
+            setCurrentGear(2);
         }else if(newSpeed >= 101 && newSpeed <= 150){
-            changeGear(3);
+            setCurrentGear(3);
         }else if(newSpeed >= 151 && newSpeed <= 200){
-            changeGear(4);
+            setCurrentGear(4);
         }else if(newSpeed >= 201 && newSpeed <= 250){
-            changeGear(5);
-        }else{
-            changeGear(6);
+            setCurrentGear(5);
+        }else if(newSpeed > 250){
+            setCurrentGear(6);
         }
         //for all other cases, change the gear accordingly
 
